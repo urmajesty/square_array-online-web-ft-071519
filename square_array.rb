d0ab6056ecddacb78a0fdb7c9b1a4778fceb9966
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-19 18:54:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,10 @@
-require 'pry'
 
 def square_array(array)
 new_array = [] 
 array.each do |number|
-  binding.pry
-  new_array([number ** 2])
-  return new_array
+  new_array.push(number **2)
+  
 
 end
-
+return new_array
 end
